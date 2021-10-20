5e7a7a772960f4e4fcee55a42085d2caeadb1eb9
Daniel Pavon gomez <dpavon-g@c1r3s3.42madrid.com>
2021-10-20 19:38:40 +0200

    Better norminette

--- charge_map.c
+++ charge_map.c
@@ -129,42 +129,47 @@ int	good_value(char *str)
 	return (flag);
 }
 
+void	save_map(t_vector	coord, t_values **maptrix, t_gdates dates, int fd)
+{
+	char	*string;
+	char	**the_split;
+
+	coord.x = 0;
+	get_next_line(fd, &string);
+	the_split = ft_split(string, ' ');
+	while (coord.x < dates.columns)
+	{
+		if (is_digit(the_split[coord.x]) == 1)
+		{
+			maptrix[coord.y][coord.x].number = ft_atoi(the_split[coord.x]);
+			maptrix[coord.y][coord.x].color = 16777215;
+		}
+		else if (good_value(the_split[coord.x]) == 1)
+		{
+			maptrix[coord.y][coord.x].number = ft_atoi(the_split[coord.x]);
+			maptrix[coord.y][coord.x].color = get_color(the_split[coord.x]);
+		}
+		else
+			dates.flag = 1;
+		free(the_split[coord.x]);
+		coord.x++;
+	}
+	free(the_split);
+	free(string);
+}
+
 int	charge_map(t_values **maptrix, t_gdates dates)
 {
-	int		fd;
-	char	*string;
+	int			fd;
 	t_vector	coord;
-	char	**the_split;
 
 	dates.flag = 0;
 	coord.y = 0;
 	fd = open(dates.map, O_RDONLY);
 	while (coord.y < dates.rows)
 	{
-		coord.x = 0;
-		get_next_line(fd, &string);
-		the_split = ft_split(string, ' ');
-		while (coord.x < dates.columns)
-		{
-			if (is_digit(the_split[coord.x]) == 1)
-			{
-				maptrix[coord.y][coord.x].number = ft_atoi(the_split[coord.x]);
-				maptrix[coord.y][coord.x].color = 16777215;
-			}
-			else if (good_value(the_split[coord.x]) == 1)
-			{
-				maptrix[coord.y][coord.x].number = ft_atoi(the_split[coord.x]);
-				maptrix[coord.y][coord.x].color = get_color(the_split[coord.x]);
-			}
-			else
-				dates.flag = 1;
-			free(the_split[coord.x]);
-			coord.x++;
-		}
+		save_map(coord, maptrix, dates, fd);
 		coord.y++;
-		free(the_split);
-		free(string);
 	}
-	ft_printf("Color: %d\n", maptrix[199][0].color);
 	return (dates.flag);
 }
