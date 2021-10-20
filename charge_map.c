85a1afa222b44251e1a43989411b33f6e005b85e
Daniel Pavon gomez <dpavon-g@c1r3s3.42madrid.com>
2021-10-20 19:18:00 +0200

    fdf working with no good norminette

--- charge_map.c
+++ charge_map.c
@@ -133,39 +133,38 @@ int	charge_map(t_values **maptrix, t_gdates dates)
 {
 	int		fd;
 	char	*string;
-	int		x;
+	t_vector	coord;
 	char	**the_split;
-	int		y;
 
 	dates.flag = 0;
-	y = 0;
+	coord.y = 0;
 	fd = open(dates.map, O_RDONLY);
-	while (y < dates.rows)
+	while (coord.y < dates.rows)
 	{
-		x = 0;
+		coord.x = 0;
 		get_next_line(fd, &string);
 		the_split = ft_split(string, ' ');
-		while (x < dates.columns)
+		while (coord.x < dates.columns)
 		{
-			if (is_digit(the_split[x]) == 1)
+			if (is_digit(the_split[coord.x]) == 1)
 			{
-				maptrix[y][x].number = ft_atoi(the_split[x]);
-				maptrix[y][x].color = 16777215;
+				maptrix[coord.y][coord.x].number = ft_atoi(the_split[coord.x]);
+				maptrix[coord.y][coord.x].color = 16777215;
 			}
-			else if (good_value(the_split[x]) == 1)
+			else if (good_value(the_split[coord.x]) == 1)
 			{
-				maptrix[y][x].number = ft_atoi(the_split[x]);
-				maptrix[y][x].color = get_color(the_split[x]);
+				maptrix[coord.y][coord.x].number = ft_atoi(the_split[coord.x]);
+				maptrix[coord.y][coord.x].color = get_color(the_split[coord.x]);
 			}
 			else
 				dates.flag = 1;
-			free(the_split[x]);
-			x++;
+			free(the_split[coord.x]);
+			coord.x++;
 		}
-		y++;
+		coord.y++;
 		free(the_split);
 		free(string);
 	}
-	ft_printf("The color: %d\n", maptrix[0][0].color);
+	ft_printf("Color: %d\n", maptrix[199][0].color);
 	return (dates.flag);
 }
